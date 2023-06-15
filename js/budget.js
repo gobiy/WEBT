4265850de9ce65200bb0d23d57fb5978295b9eaf
gobiy <y-gobitha@hotmail.com>
2023-06-15 13:21:13 +0200

    Canva and List function :)

--- js/budget.js
+++ js/budget.js
@@ -1,58 +1,87 @@
 function displayTransactions(transactions) {
-    const transactionList = document.getElementById('transaction-list');
-    const totalAmountElement = document.getElementById('total-amount');
-    let totalAmount = 0;
+const totalIncomeInput = document.getElementById('einnahmen');
+const transactionList = document.getElementById('transaction-list');
+const totalAmountElement = document.getElementById('total-amount');
+let totalAmount = 0;
 
-    transactions.forEach((transaction) => {
-        const { Kategorie, Betrag, Beschreibung } = transaction;
+transactions.forEach((transaction) => {
+    const { Kategorie, Betrag, Beschreibung } = transaction;
 
-        const transactionItem = document.createElement('li');
-        transactionItem.textContent = `Kategorie: ${Kategorie}, Betrag: ${Betrag}, Beschreibung: ${Beschreibung}`;
+    const transactionItem = document.createElement('li');
+    transactionItem.textContent = `Kategorie: ${Kategorie}, Betrag: ${Betrag}, Beschreibung: ${Beschreibung}`;
+    transactionItem.setAttribute('data-amount', Betrag);
 
-        transactionList.appendChild(transactionItem);
+    transactionList.appendChild(transactionItem);
 
-        totalAmount += parseFloat(Betrag);
-    });
+    totalAmount += parseFloat(Betrag);
+});
 
-    totalAmountElement.textContent = `Gesamtbetrag: ${totalAmount.toFixed(2)}`;
+totalAmountElement.textContent = `Gesamtbetrag: ${totalAmount.toFixed(2)}`;
+updateTotalAmount();
+}
+
+function createExpenseChart(transactions) {
+const ctx = document.getElementById('canvas').getContext('2d');
+
+const categories = {};
+transactions.forEach((transaction) => {
+    const { Kategorie, Betrag } = transaction;
+    if (categories[Kategorie]) {
+    categories[Kategorie] += parseFloat(Betrag);
+    } else {
+    categories[Kategorie] = parseFloat(Betrag);
+    }
+});
+
+const labels = Object.keys(categories);
+const data = Object.values(categories);
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+    labels: labels,
+    datasets: [{
+        data: data,
+        backgroundColor: [
+        '#ff6384',
+        '#36a2eb',
+        '#ffce56',
+        '#8eff8e',
+        '#f5a3ff',
+        '#b2b2b2',
+        '#ffd54c',
+        ],
+    }],
+    },
+});
+}
+
+function displayTransactions(transactions) {
+const totalIncomeInput = document.getElementById('einnahmen');
+const transactionList = document.getElementById('transaction-list');
+const totalAmountElement = document.getElementById('total-amount');
+let totalAmount = 0;
+
+transactions.forEach((transaction) => {
+    const { Kategorie, Betrag, Beschreibung } = transaction;
+
+    const transactionItem = document.createElement('li');
+    transactionItem.textContent = `Kategorie: ${Kategorie}, Betrag: ${Betrag}, Beschreibung: ${Beschreibung}`;
+    transactionItem.setAttribute('data-amount', Betrag);
+
+    transactionList.appendChild(transactionItem);
+
+    totalAmount += parseFloat(Betrag);
+    
+});
+
+totalAmountElement.textContent = `Gesamtbetrag: ${totalAmount.toFixed(2)}`;
+
+createExpenseChart(transactions);
 }
 
 fetch("add_expense.php")
 .then((response) => response.json())
 .then((json) => displayTransactions(json));
 
-
-
-
-
-/*document.addEventListener('DOMContentLoaded', function() {
-    let addedCategories = [];
-
-    if (localStorage.getItem('addedCategories')) {
-        addedCategories = JSON.parse(localStorage.getItem('addedCategories'));
-        updateCategoryOptions();
-    }
-
-    const form = document.getElementById('expense-form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        const category = document.getElementById('category').value;
-        const amount = parseFloat(document.getElementById('amount').value);
-        const description = document.getElementById('description').value;
-
-        addExpenseToCanvas(category, amount);
-
-        displayTransaction(category, amount, description);
-
-        if (!addedCategories.includes(category)) {
-            addedCategories.push(category);
-            localStorage.setItem('addedCategories', JSON.stringify(addedCategories));
-        }
-
-        updateCategoryOptions();
-
-        form.reset();
-    });
-
-});*/
+updateTotalAmount();
--- js/script.js
+++ js/script.js
@@ -1,85 +1,79 @@
 /*document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('expense-form');
-  form.addEventListener('submit', function(event) {
-      event.preventDefault();
-
-      const category = document.getElementById('category').value;
-      const amount = parseFloat(document.getElementById('amount').value);
-      const description = document.getElementById('description').value;
-
-      if (!category || !amount) {
-          return; 
-      }
-
-      addExpenseToCanvas(category, amount);
-
-      displayTransaction(category, amount, description);
-
-      form.reset();
-  });
-
-  const addCategoryButton = document.getElementById('add-category');
-  addCategoryButton.addEventListener('click', function() {
-      const newCategory = document.getElementById('new-category').value;
-      if (newCategory) {
-          addCategoryOption(newCategory);
-          document.getElementById('new-category').value = '';
-      }
-  });
-
-  function addCategoryOption(category) {
-      const selectElement = document.getElementById('category');
-      const newOption = document.createElement('option');
-      newOption.value = category;
-      newOption.textContent = category;
-      selectElement.appendChild(newOption);
-  }
-
-  const chartData = [];
-
+    const form = document.getElementById('expense-form');
+    const chartData = [];
+  
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const category = document.getElementById('category').value;
+        const amount = parseFloat(document.getElementById('amount').value);
+        
+        addExpenseToCanvas(category, amount);
+        resetForm();
+    });
+  
     function addExpenseToCanvas(category, amount) {
-      const existingCategory = chartData.find(item => item.category === category);
-
-      if (existingCategory) {
-          existingCategory.amount += amount;
-      } else {
-          chartData.push({ category: category, amount: amount });
-      }
-
-      drawPieChart(chartData);
-  }
-
-  function drawPieChart(data) {
-      const canvas = document.getElementById('canvas');
-      const context = canvas.getContext('2d');
-      const centerX = canvas.width / 2;
-      const centerY = canvas.height / 2;
-      const radius = Math.min(centerX, centerY) - 10;
-      const total = data.reduce((sum, item) => sum + item.amount, 0);
-      let startAngle = 0;
-
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
-      data.forEach(item => {
-          const sliceAngle = (item.amount / total) * 2 * Math.PI;
-
-          context.beginPath();
-          context.moveTo(centerX, centerY);
-          context.arc(centerX, centerY, radius, startAngle, startAngle + sliceAngle);
-          context.fillStyle = getRandomColor();
-          context.fill();
-
-          startAngle += sliceAngle;
-      });
-  }
-
-   function getRandomColor() {
-      const letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-  }
+        const existingCategory = chartData.find(item => item.category === category);
+  
+        if (existingCategory) {
+            existingCategory.amount += amount;
+        } else {
+            chartData.push({ category: category, amount: amount });
+        }
+  
+        drawPieChart(chartData);
+    }
+  
+    function drawPieChart(data) {
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+        const centerX = canvas.width / 2;
+        const centerY = canvas.height / 2;
+        const radius = Math.min(centerX, centerY) - 10;
+        const total = data.reduce((sum, item) => sum + item.amount, 0);
+        let startAngle = 0;
+  
+        context.clearRect(0, 0, canvas.width, canvas.height);
+  
+        data.forEach(item => {
+            const sliceAngle = (item.amount / total) * 2 * Math.PI;
+  
+            context.beginPath();
+            context.moveTo(centerX, centerY);
+            context.arc(centerX, centerY, radius, startAngle, startAngle + sliceAngle);
+            context.fillStyle = getRandomColor();
+            context.fill();
+  
+            startAngle += sliceAngle;
+        });
+  
+        displayTransactions(chartData);
+    }
+  
+    }
+  
+    function resetForm() {
+        document.getElementById('category').value = '';
+        document.getElementById('amount').value = '';
+        document.getElementById('description').value = '';
+    }
+  
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+  
+    // Lade vorhandene Transaktionen beim Laden der Seite
+    fetch('add_expense.php')
+        .then(response => response.json())
+        .then(json => {
+            chartData.push(...json);
+            drawPieChart(chartData);
+            displayTransactions(chartData);
+        })
+        .catch(error => console.error('Fehler beim Laden der Transaktionen:', error));
 });
-*/
