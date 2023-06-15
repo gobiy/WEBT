dc7f57f0f244bdaace38aa35088d618cb7a68e76
gobiy <y-gobitha@hotmail.com>
2023-06-15 14:38:59 +0200

    problem with gesamtausgaben

--- js/budget.js
+++ js/budget.js
@@ -1,87 +1,67 @@
-function displayTransactions(transactions) {
-const totalIncomeInput = document.getElementById('einnahmen');
-const transactionList = document.getElementById('transaction-list');
-const totalAmountElement = document.getElementById('total-amount');
-let totalAmount = 0;
-
-transactions.forEach((transaction) => {
-    const { Kategorie, Betrag, Beschreibung } = transaction;
-
-    const transactionItem = document.createElement('li');
-    transactionItem.textContent = `Kategorie: ${Kategorie}, Betrag: ${Betrag}, Beschreibung: ${Beschreibung}`;
-    transactionItem.setAttribute('data-amount', Betrag);
-
-    transactionList.appendChild(transactionItem);
-
-    totalAmount += parseFloat(Betrag);
-});
-
-totalAmountElement.textContent = `Gesamtbetrag: ${totalAmount.toFixed(2)}`;
-updateTotalAmount();
-}
-
 function createExpenseChart(transactions) {
-const ctx = document.getElementById('canvas').getContext('2d');
+    const ctx = document.getElementById('canvas').getContext('2d');
 
-const categories = {};
-transactions.forEach((transaction) => {
-    const { Kategorie, Betrag } = transaction;
-    if (categories[Kategorie]) {
-    categories[Kategorie] += parseFloat(Betrag);
-    } else {
-    categories[Kategorie] = parseFloat(Betrag);
-    }
-});
+    const categories = {};
+    transactions.forEach((transaction) => {
+        const { Kategorie, Betrag } = transaction;
+        if (categories[Kategorie]) {
+            categories[Kategorie] += parseFloat(Betrag);
+        } else {
+            categories[Kategorie] = parseFloat(Betrag);
+        }
+    });
 
-const labels = Object.keys(categories);
-const data = Object.values(categories);
+    const labels = Object.keys(categories);
+    const data = Object.values(categories);
 
-new Chart(ctx, {
-    type: 'pie',
-    data: {
-    labels: labels,
-    datasets: [{
-        data: data,
-        backgroundColor: [
-        '#ff6384',
-        '#36a2eb',
-        '#ffce56',
-        '#8eff8e',
-        '#f5a3ff',
-        '#b2b2b2',
-        '#ffd54c',
-        ],
-    }],
-    },
-});
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    '#ff6384',
+                    '#36a2eb',
+                    '#ffce56',
+                    '#8eff8e',
+                    '#f5a3ff',
+                    '#b2b2b2',
+                    '#ffd54c',
+                ],
+            }],
+        },
+    });
 }
 
 function displayTransactions(transactions) {
-const totalIncomeInput = document.getElementById('einnahmen');
-const transactionList = document.getElementById('transaction-list');
-const totalAmountElement = document.getElementById('total-amount');
-let totalAmount = 0;
+    const einnahmenInput = document.getElementById('einnahmen');
+    const transactionList = document.getElementById('transaction-list');
+    const totalAmountElement = document.getElementById('total-amount');
+    const gesamtausgabenElement = document.getElementById('gesamtausgaben');
+    let totalAmount = 0;
 
-transactions.forEach((transaction) => {
-    const { Kategorie, Betrag, Beschreibung } = transaction;
+    transactions.forEach((transaction) => {
+        const { Kategorie, Betrag, Beschreibung } = transaction;
 
-    const transactionItem = document.createElement('li');
-    transactionItem.textContent = `Kategorie: ${Kategorie}, Betrag: ${Betrag}, Beschreibung: ${Beschreibung}`;
-    transactionItem.setAttribute('data-amount', Betrag);
+        const transactionItem = document.createElement('li');
+        transactionItem.textContent = `Einnahmen: ${einnahmenInput.value}, Kategorie: ${Kategorie}, Betrag: ${Betrag}, Beschreibung: ${Beschreibung}`;
+        transactionItem.setAttribute('data-amount', Betrag);
 
-    transactionList.appendChild(transactionItem);
+        transactionList.appendChild(transactionItem);
 
-    totalAmount += parseFloat(Betrag);
-    
-});
+        totalAmount += parseFloat(Betrag);
+    });
 
-totalAmountElement.textContent = `Gesamtbetrag: ${totalAmount.toFixed(2)}`;
+    totalAmountElement.textContent = `Gesamtbetrag: ${totalAmount.toFixed(2)}`;
 
-createExpenseChart(transactions);
+    const einnahmenValue = parseFloat(einnahmenInput.value) || 0;
+    const gesamtausgaben = einnahmenValue - totalAmount;
+    gesamtausgabenElement.textContent = `Gesamtausgaben: ${gesamtausgaben.toFixed(2)}`;
+
+    createExpenseChart(transactions);
 }
 
 fetch("add_expense.php")
-.then((response) => response.json())
-.then((json) => displayTransactions(json));
-
-updateTotalAmount();
+    .then((response) => response.json())
+    .then((json) => displayTransactions(json));
